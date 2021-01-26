746f2a9541e5b8382c515a9868bb3b366548c5d5
Ioan Crișan <3511581+ioancrisan@users.noreply.github.com>
2021-01-26 19:03:26 +0200

    added first draft of @kephas/angular-oidc

---
+++ angular/projects/angular-oidc/README.md
@@ -0,0 +1,3 @@
+# @kephas/angular-oidc
+
+Provides the integration of the OIDC API with Kephas and Angular.
---
+++ angular/projects/angular-oidc/src/public-api.ts
@@ -0,0 +1,7 @@
+/*
+ * Public API Surface of angular-oidc
+ */
+
+export * from './lib/authorization.settings';
+export * from './lib/authorize.service';
+export * from './lib/authorize.interceptor';

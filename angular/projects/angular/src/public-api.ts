8d2cb6013d2f8a75c6f143cbae462a3a4d8d9cfe
Ioan Crișan <3511581+ioancrisan@users.noreply.github.com>
2021-01-26 17:22:15 +0200

    Removed security from @kephas/angular and moved to @kephas/angular-oidc

--- angular/projects/angular/src/public-api.ts
+++ angular/projects/angular/src/public-api.ts
@@ -13,8 +13,4 @@ export * from './lib/services/appSettings';
 export * from './lib/services/httpCommandProcessorClient';
 export * from './lib/services/httpMessageProcessorClient';
 
-export * from './lib/services/security/token.service';
-export * from './lib/services/security/token.interceptor';
-export * from './lib/services/security/jwt.interceptor';
-
 export * from './lib/services/configuration';

faa8d31322dc40254d4f439bf8c4e230a8c1de5a
Ioan Crișan <3511581+ioancrisan@users.noreply.github.com>
2022-02-11 12:39:56 +0200

    Try fix httpCommandProcessor

--- src/projects/kephas/ngx-core/src/lib/services/httpCommandProcessorClient.ts
+++ src/projects/kephas/ngx-core/src/lib/services/httpCommandProcessorClient.ts
@@ -1,10 +1,10 @@
 import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import {
-    LogLevel, AppService,
-    Priority, Logger, Expando
+  LogLevel, AppService,
+  Priority, Logger, Expando
 } from '@kephas/core';
 import {
-    CommandProcessorClient, CommandClientContext, CommandResponse, CommandError
+  CommandProcessorClient, CommandClientContext, CommandResponse, CommandError
 } from '@kephas/commands';
 import { NotificationService } from '@kephas/ui';
 import { Observable, ObservableInput } from 'rxjs';
@@ -20,159 +20,173 @@ import { AppSettings } from './appSettings';
 @AppService({ overridePriority: Priority.Low })
 export class HttpCommandProcessorClient extends CommandProcessorClient {
 
-    /**
-     * Gets or sets the base route for the command execution.
-     *
-     * @protected
-     * @type {string}
-     * @memberof CommandProcessor
-     */
-    protected baseRoute = 'api/cmd/';
+  /**
+   * Gets or sets the base route for the command execution.
+   *
+   * @protected
+   * @type {string}
+   * @memberof CommandProcessor
+   */
+  protected baseRoute = 'api/cmd/';
 
-    /**
-     * Initializes a new instance of the CommandProcessor class.
-     * @param {NotificationService} notification The notification service.
-     * @param {HttpClient} http The HTTP client.
-     * @param {AppSettings} appSettings The application settings.
-     */
-    constructor(
-        protected appSettings: AppSettings,
-        protected http: HttpClient,
-        protected notification: NotificationService,
-        protected logger: Logger) {
-        super();
+  /**
+   * Initializes a new instance of the CommandProcessor class.
+   * @param {NotificationService} notification The notification service.
+   * @param {HttpClient} http The HTTP client.
+   * @param {AppSettings} appSettings The application settings.
+   */
+  constructor(
+    protected appSettings: AppSettings,
+    protected http: HttpClient,
+    protected notification: NotificationService,
+    protected logger: Logger) {
+    super();
+  }
+
+  /**
+   * Processes the command asynchronously.
+   * @tparam T The command response type.
+   * @param {string} command The command.
+   * @param {{}} [args] Optional. The arguments.
+   * @param {CommandClientContext} [options] Optional. Options controlling the command processing.
+   * @returns {Observable{T}} An observable over the result.
+   */
+  public process<T extends CommandResponse>(command: string, args?: {}, options?: CommandClientContext): Observable<T> {
+    const url = this.getHttpGetUrl(command, args, options);
+    let obs = this.http.get<T>(url, this.getHttpGetOptions(command, args, options));
+    if (options && options.retries) {
+      obs = obs.pipe(
+        retry(options.retries),
+        map(response => this._processResponse(response, options)),
+        catchError(error => this._processError<T>(error, options)));
+    }
+    else {
+      obs = obs.pipe(
+        map(response => this._processResponse(response, options)),
+        catchError(error => this._processError<T>(error, options)));
     }
 
-    /**
-     * Processes the command asynchronously.
-     * @tparam T The command response type.
-     * @param {string} command The command.
-     * @param {{}} [args] Optional. The arguments.
-     * @param {CommandClientContext} [options] Optional. Options controlling the command processing.
-     * @returns {Observable{T}} An observable over the result.
-     */
-    public process<T extends CommandResponse>(command: string, args?: {}, options?: CommandClientContext): Observable<T> {
-        const url = this.getHttpGetUrl(command, args, options);
-        let obs = this.http.get<T>(url, this.getHttpGetOptions(command, args, options));
-        if (options && options.retries) {
-            obs = obs.pipe(
-                retry(options.retries),
-                map(response => this._processResponse(response, options)),
-                catchError(error => this._processError<T>(error, options)));
-        }
-        else {
-            obs = obs.pipe(
-                map(response => this._processResponse(response, options)),
-                catchError(error => this._processError<T>(error, options)));
-        }
+    return obs;
+  }
 
-        return obs;
+  /**
+   * Gets the HTTP GET URL.
+   *
+   * @protected
+   * @param {string} command The command.
+   * @param {{}} [args] Optional. The arguments.
+   * @param {CommandClientContext} [options] Optional. Options controlling the command processing.
+   * @returns {string} The HTTP GET URL.
+   * @memberof CommandProcessor
+   */
+  protected getHttpGetUrl(command: string, args?: {}, options?: CommandClientContext): string {
+    let baseUrl = this.appSettings.baseUrl;
+    if (!baseUrl.endsWith('/')) {
+      baseUrl = baseUrl + '/';
     }
 
-    /**
-     * Gets the HTTP GET URL.
-     *
-     * @protected
-     * @param {string} command The command.
-     * @param {{}} [args] Optional. The arguments.
-     * @param {CommandClientContext} [options] Optional. Options controlling the command processing.
-     * @returns {string} The HTTP GET URL.
-     * @memberof CommandProcessor
-     */
-    protected getHttpGetUrl(command: string, args?: {}, options?: CommandClientContext): string {
-        let baseUrl = this.appSettings.baseUrl;
-        if (!baseUrl.endsWith('/')) {
-            baseUrl = baseUrl + '/';
-        }
+    return `${baseUrl}${this.baseRoute}${command}`;
+  }
 
-        return `${baseUrl}${this.baseRoute}${command}`;
+  /**
+   * Gets the command GET parameters.
+   *
+   * @protected
+   * @param {{}} [args]
+   * @param {CommandClientContext} [options]
+   * @return {*}  {HttpParams}
+   * @memberof HttpCommandProcessorClient
+   */
+  protected getHttpGetParams(args?: {}, options?: CommandClientContext): HttpParams | {
+    [param: string]: string | string[];
+  } {
+    let params: Expando = {};
+    if (args) {
+      Object.keys(args)
+        .forEach(key => params[key] = this._getParamValue((args as Expando)[key]));
     }
 
-    /**
-     * Gets the command GET parameters.
-     *
-     * @protected
-     * @param {{}} [args]
-     * @param {CommandClientContext} [options]
-     * @return {*}  {HttpParams}
-     * @memberof HttpCommandProcessorClient
-     */
-    protected getHttpGetParams(args?: {}, options?: CommandClientContext): HttpParams {
-      let params = new HttpParams();
-      if (args) {
-        Object.keys(args)
-            .forEach(key => params.set(key, (args as Expando)[key]));
+    return params;
+  }
+
+  /**
+   * Gets the HTTP GET options. By default it does not return any options.
+   *
+   * @protected
+   * @param {string} command The command.
+   * @param {{}} [args] Optional. The arguments.
+   * @param {CommandClientContext} [options] Optional. Options controlling the command processing.
+   * @returns {({
+   *             headers?: HttpHeaders | {
+   *                 [header: string]: string | string[];
+   *             };
+   *             observe?: 'body';
+   *             params?: HttpParams | {
+   *                 [param: string]: string | string[];
+   *             };
+   *             reportProgress?: boolean;
+   *             responseType?: 'json';
+   *             withCredentials?: boolean;
+   *         } | undefined)} The options or undefined.
+   * @memberof CommandProcessor
+   */
+  protected getHttpGetOptions(command: string, args?: {}, options?: CommandClientContext): {
+    headers?: HttpHeaders | {
+      [header: string]: string | string[];
+    };
+    observe?: 'body';
+    params?: HttpParams | {
+      [param: string]: string | string[];
+    };
+    reportProgress?: boolean;
+    responseType?: 'json';
+    withCredentials?: boolean;
+  } | undefined {
+    return {
+      params: this.getHttpGetParams(args, options),
+    };
+  }
+
+  private _processResponse<T extends CommandResponse>(response: T, options?: CommandClientContext): T {
+    if (typeof response.severity === 'string') {
+      response.severity = (LogLevel as Expando)[response.severity as string];
+    }
+
+    if (response.severity <= LogLevel.Error) {
+      throw new CommandError(response.message!, response);
+    }
+
+    if (response.severity === LogLevel.Warning) {
+      this.logger.log(response.severity, null, response.message!);
+      if (!(options && (options.notifyWarnings === undefined || options.notifyWarnings))) {
+        this.notification.notifyWarning(response);
       }
-
-      return params;
     }
 
-    /**
-     * Gets the HTTP GET options. By default it does not return any options.
-     *
-     * @protected
-     * @param {string} command The command.
-     * @param {{}} [args] Optional. The arguments.
-     * @param {CommandClientContext} [options] Optional. Options controlling the command processing.
-     * @returns {({
-     *             headers?: HttpHeaders | {
-     *                 [header: string]: string | string[];
-     *             };
-     *             observe?: 'body';
-     *             params?: HttpParams | {
-     *                 [param: string]: string | string[];
-     *             };
-     *             reportProgress?: boolean;
-     *             responseType?: 'json';
-     *             withCredentials?: boolean;
-     *         } | undefined)} The options or undefined.
-     * @memberof CommandProcessor
-     */
-    protected getHttpGetOptions(command: string, args?: {}, options?: CommandClientContext): {
-        headers?: HttpHeaders | {
-            [header: string]: string | string[];
-        };
-        observe?: 'body';
-        params?: HttpParams | {
-            [param: string]: string | string[];
-        };
-        reportProgress?: boolean;
-        responseType?: 'json';
-        withCredentials?: boolean;
-    } | undefined {
-        return {
-          params: this.getHttpGetParams(args, options),
-        };
+    if (response.severity <= LogLevel.Error) {
+      throw new Error(response.message);
+    }
+    return response;
+  }
+
+  private _processError<T extends CommandResponse>(error: any, options?: CommandClientContext): ObservableInput<T> {
+    this.logger.error(error);
+    if (!(options && (options.notifyErrors === undefined || options.notifyErrors))) {
+      this.notification.notifyError(error);
     }
 
-    private _processResponse<T extends CommandResponse>(response: T, options?: CommandClientContext): T {
-        if (typeof response.severity === 'string') {
-            response.severity = (LogLevel as Expando)[response.severity as string];
-        }
+    throw error;
+  }
 
-        if (response.severity <= LogLevel.Error) {
-            throw new CommandError(response.message!, response);
-        }
-
-        if (response.severity === LogLevel.Warning) {
-            this.logger.log(response.severity, null, response.message!);
-            if (!(options && (options.notifyWarnings === undefined || options.notifyWarnings))) {
-                this.notification.notifyWarning(response);
-            }
-        }
-
-        if (response.severity <= LogLevel.Error) {
-            throw new Error(response.message);
-        }
-        return response;
+  private _getParamValue(rawValue: any): string | string[] {
+    if (typeof rawValue === 'string') {
+      return rawValue;
     }
 
-    private _processError<T extends CommandResponse>(error: any, options?: CommandClientContext): ObservableInput<T> {
-        this.logger.error(error);
-        if (!(options && (options.notifyErrors === undefined || options.notifyErrors))) {
-            this.notification.notifyError(error);
-        }
-
-        throw error;
+    if (Array.isArray(rawValue)) {
+      return rawValue.map(e => `${e}`);
     }
+
+    return `${rawValue}`;
+  }
 }

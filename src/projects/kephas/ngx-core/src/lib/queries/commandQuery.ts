810bca91bbae274ad48e2e9e0e540039181280be
Ioan Crișan <3511581+ioancrisan@users.noreply.github.com>
2022-03-09 15:04:53 +0200

    Improve the command and message query

--- src/projects/kephas/ngx-core/src/lib/queries/commandQuery.ts
+++ src/projects/kephas/ngx-core/src/lib/queries/commandQuery.ts
@@ -8,10 +8,35 @@ export interface CommandState<TArgs> extends Expando {
   args?: TArgs;
 }
 
+/**
+ * Observable based on a command query.
+ *
+ * @export
+ * @abstract
+ * @class CommandQuery
+ * @extends {BehaviorSubject<TValue>}
+ * @template TArgs
+ * @template TResponseMessage
+ * @template TValue
+ */
 export abstract class CommandQuery<TArgs, TResponseMessage, TValue> extends BehaviorSubject<TValue> {
-  public loading: boolean = false;
+  static #execId = 0;
+  static #pendingExecs: Expando = {};
+
+  #loading = false;
+  #lastError: any;
+
   public args?: TArgs;
 
+  /**
+   * Creates an instance of CommandQuery.
+   * @param {CommandProcessorClient} processor
+   * @param {string} command
+   * @param {(r: TResponseMessage, state?: CommandState<TArgs>) => TValue} [resultMap]
+   * @param {(args?: TArgs, state?: CommandState<TArgs>) => TArgs} [argsMap]
+   * @param {TValue} [emptyResult={} as TValue]
+   * @memberof CommandQuery
+   */
   constructor(
     protected readonly processor: CommandProcessorClient,
     protected command: string,
@@ -22,22 +47,57 @@ export abstract class CommandQuery<TArgs, TResponseMessage, TValue> extends Beha
     this.args ??= {} as TArgs;
   }
 
+  /**
+   * Gets a value indicating whether the query is executing.
+   *
+   * @readonly
+   * @type {boolean}
+   * @memberof CommandQuery
+   */
+  get loading(): boolean {
+    return this.#loading;
+  }
+
+  /**
+   * Gets the last error executing this query.
+   *
+   * @readonly
+   * @type {*}
+   * @memberof CommandQuery
+   */
+  get lastError(): any {
+    return this.#lastError;
+  }
+
+  /**
+   * Executes the command asynchronously.
+   *
+   * @param {CommandState<TArgs>} [state]
+   * @memberof CommandQuery
+   */
   public execute(state?: CommandState<TArgs>): void {
-    this.fetch(state?.command ?? this.command, state?.args ?? this.args!, state)
-      .pipe(tap(x => super.next(x)));
+    const execId = (CommandQuery.#execId++).toString();
+    CommandQuery.#pendingExecs[execId] = this.fetch(state?.command ?? this.command, state?.args ?? this.args!, state)
+      .pipe(
+        tap(x => super.next(x)),
+        finalize(() => delete CommandQuery.#pendingExecs[execId]));
   }
 
   protected fetch(command: string, args?: TArgs, state?: CommandState<TArgs>): Observable<TValue> {
-    this.loading = true;
+    this.#loading = true;
 
     args = this.mapArgs(args, state);
 
     return this.processor
       .process(command, args!)
       .pipe(
-        map(response => this.mapResponse(response as unknown as TResponseMessage, state)),
-        finalize(() => this.loading = false),
-        catchError(_ => {
+        map(response => {
+          this.#lastError = undefined;
+          return this.mapResponse(response as unknown as TResponseMessage, state);
+        }),
+        finalize(() => this.#loading = false),
+        catchError(err => {
+          this.#lastError = err;
           return of(this.emptyResult);
         })
       );
--- src/projects/kephas/ngx-core/src/lib/queries/messageQuery.ts
+++ src/projects/kephas/ngx-core/src/lib/queries/messageQuery.ts
@@ -7,9 +7,32 @@ export interface MessageState<TMessage> extends Expando {
   message?: TMessage;
 }
 
+/**
+ * Observable based on a message query.
+ *
+ * @export
+ * @abstract
+ * @class MessageQuery
+ * @extends {BehaviorSubject<TValue>}
+ * @template TMessage
+ * @template TResponseMessage
+ * @template TValue
+ */
 export abstract class MessageQuery<TMessage, TResponseMessage extends ResponseMessage, TValue> extends BehaviorSubject<TValue> {
-  public loading: boolean = false;
+  static #execId = 0;
+  static #pendingExecs: Expando = {};
 
+  #loading = false;
+  #lastError: any;
+
+  /**
+   * Creates an instance of MessageQuery.
+   * @param {MessageProcessorClient} processor
+   * @param {(r: TResponseMessage, state?: MessageState<TMessage>) => TValue} [resultMap]
+   * @param {(m: TMessage, state?: MessageState<TMessage>) => TMessage} [messageMap]
+   * @param {TValue} [emptyResult={} as TValue]
+   * @memberof MessageQuery
+   */
   constructor(
     protected readonly processor: MessageProcessorClient,
     protected resultMap?: (r: TResponseMessage, state?: MessageState<TMessage>) => TValue,
@@ -18,22 +41,51 @@ export abstract class MessageQuery<TMessage, TResponseMessage extends ResponseMe
     super(null!);
   }
 
+  /**
+   * Gets a value indicating whether the query is executing.
+   *
+   * @readonly
+   * @type {boolean}
+   * @memberof CommandQuery
+   */
+  get loading(): boolean {
+    return this.#loading;
+  }
+
+  /**
+   * Gets the last error executing this query.
+   *
+   * @readonly
+   * @type {*}
+   * @memberof CommandQuery
+   */
+  get lastError(): any {
+    return this.#lastError;
+  }
+
   public execute(state?: MessageState<TMessage>): void {
-    this.fetch(state?.message, state)
-      .pipe(tap(x => super.next(x)));
+    const execId = (MessageQuery.#execId++).toString();
+    MessageQuery.#pendingExecs[execId] = this.fetch(state?.message, state)
+      .pipe(
+        tap(x => super.next(x)),
+        finalize(() => delete MessageQuery.#pendingExecs[execId]));
   }
 
   protected fetch(message: any, state?: MessageState<TMessage>): Observable<TValue> {
-    this.loading = true;
+    this.#loading = true;
 
     message = this.mapMessage(message, state);
 
     return this.processor
       .process(message)
       .pipe(
-        map(response => this.mapResponse(response as TResponseMessage, state)),
-        finalize(() => this.loading = false),
-        catchError(_ => {
+        map(response => {
+          this.#lastError = undefined;
+          return this.mapResponse(response as unknown as TResponseMessage, state);
+        }),
+        finalize(() => this.#loading = false),
+        catchError(err => {
+          this.#lastError = err;
           return of(this.emptyResult);
         })
       );
